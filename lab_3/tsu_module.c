035641308b9b09b44e9afe0c81898f49982e6135
katet_3 <colpakov.vadik@yandex.ru>
2023-12-13 13:10:52 +0700

    lab_3

---
+++ lab_3/tsu_module.c
@@ -0,0 +1,19 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Your Name");
+MODULE_DESCRIPTION("Tomsk State University Module");
+
+static int __init tsu_module_init(void) {
+    printk(KERN_INFO "Welcome to the Tomsk State University\n");
+    return 0;
+}
+
+static void __exit tsu_module_exit(void) {
+    printk(KERN_INFO "Tomsk State University forever!\n");
+}
+
+module_init(tsu_module_init);
+module_exit(tsu_module_exit);
